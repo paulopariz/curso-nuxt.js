be5ccf2ce10e481350f87c404fd999f7fe72c01f
paulopariz <paulopariz01@gmail.com>
2023-02-28 20:15:47 -0300

    aula-1 / paginas e rotas 1

---
+++ aula-1/pages/servicos.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="bg-gray-100 p-3 gap-6 flex item center">
+        <nuxt-link to="/empresa">Empresa</nuxt-link>
+        <nuxt-link to="/servicos">Serviços</nuxt-link>
+    </div>
+    <div class=" container p-4 m-auto">
+      <h1 class="text-lg text-black font-semibold">Serviços</h1>
+      <p class="text-gray-600 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore explicabo maxime vero sequi ab nulla sunt, distinctio iste. Ab quaerat harum qui necessitatibus voluptate natus, magni maxime veniam totam saepe!</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
